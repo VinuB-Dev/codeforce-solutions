cb11db1a34e99ee3cb565c2b410d3286fb8904a9
Vignesh B S <55937928+vignesh-bhat1999@users.noreply.github.com>
2020-10-06 06:54:04 +0530

    Add files via upload

--- pointers.cpp
+++ pointers.cpp
@@ -1,15 +1,18 @@
 using namespace std;
 #include <iostream>
 
+//node structure
 struct Node{
   int data;
   Node* link;
 };
 
+
 void newNode(Node** head, int x){
-  struct Node* temp = new Node();
+  struct Node* temp = new Node();//creating new node
   temp->data = x;
   temp->link = NULL;
+  //updating new node
   if(*head!=NULL)temp->link = *head;
   *head = temp;
 }
